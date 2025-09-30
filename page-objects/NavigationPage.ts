31a94f4a10fe8e463c8521ca6ce88efb1145aab1
MohammedJafferAli <jaffercharliebe@gmail.com>
2025-09-30 14:55:51 +0530

    Optimised Navigation page

--- page-objects/NavigationPage.ts
+++ page-objects/NavigationPage.ts
@@ -9,27 +9,41 @@ export class NavigationPage {
     }
 
     async navigateToFormLayoutPage() {
-        await this.page.getByText("Forms").click();
+        // await this.page.getByText("Forms").click();
+        await this.selectGroupMenuItem("Forms");
+        await this.page.waitForTimeout(1000);
         await this.page.getByText("Form Layouts").click();
     }
 
     async navigateToDatepickerPage() {
-        await this.page.getByText("Forms").click();
+        // await this.page.getByText("Forms").click();
+        await this.selectGroupMenuItem("Forms");
+        await this.page.waitForTimeout(1000);
         await this.page.getByText("Datepicker").click();
     }
-    
+
     async navigateToSmartTablePage() {
-        await this.page.getByText("Tables & Data").click();
+        // await this.page.getByText("Tables & Data").click();
+        await this.selectGroupMenuItem("Tables & Data");
         await this.page.getByText("Smart Table").click();
     }
     async navigateToTooltipPage() {
-        await this.page.getByText("Modal & Overlays").click();
+        //await this.page.getByText("Modal & Overlays").click();
+        await this.selectGroupMenuItem("Modal & Overlays");
         await this.page.getByText("Tooltip").click();
     }
-    
+
     async navigateToToastPage() {
-        await this.page.getByText("Modal & Overlays").click();
+        await this.selectGroupMenuItem("Modal & Overlays");
         await this.page.getByText("Toastr").click();
+    }
+
+    private async selectGroupMenuItem(pageTitle: string) {
+        const groupMenuItem = this.page.getByTitle(pageTitle);
+        const expandedState = await groupMenuItem.getAttribute('aria-expanded');
+        if (expandedState !== 'true') {
+            await groupMenuItem.click();
+        }
     }
 
 }
