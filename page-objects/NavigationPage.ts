760f671f6cdb3eb5adf5b27b6018ea0471ee4052
MohammedJafferAli <jaffercharliebe@gmail.com>
2025-09-30 20:33:11 +0530

    Separate the locators step1

--- page-objects/NavigationPage.ts
+++ page-objects/NavigationPage.ts
@@ -1,41 +1,51 @@
-import { Page } from "playwright";
+import { Locator, Page } from "playwright";
 
 export class NavigationPage {
 
     private readonly page: Page;
+    readonly formLayoutMenuItem : Locator
+    readonly datepickerMenuItem : Locator
+    readonly smartTableMenuItem : Locator
+    readonly tooltipMenuItem : Locator
+    readonly toastMenuItem : Locator
 
     constructor(page: Page) {
         this.page = page;
+        this.formLayoutMenuItem = page.getByText("Form Layouts");
+        this.datepickerMenuItem = page.getByText("Datepicker");
+        this.smartTableMenuItem = page.getByText("Smart Table");
+        this.tooltipMenuItem = page.getByText("Tooltip");
+        this.toastMenuItem = page.getByText("Toastr");
     }
 
     async navigateToFormLayoutPage() {
         // await this.page.getByText("Forms").click();
         await this.selectGroupMenuItem("Forms");
         await this.page.waitForTimeout(1000);
-        await this.page.getByText("Form Layouts").click();
+        await this.formLayoutMenuItem.click();
     }
 
     async navigateToDatepickerPage() {
         // await this.page.getByText("Forms").click();
         await this.selectGroupMenuItem("Forms");
         await this.page.waitForTimeout(1000);
-        await this.page.getByText("Datepicker").click();
+        await this.datepickerMenuItem.click();
     }
 
     async navigateToSmartTablePage() {
         // await this.page.getByText("Tables & Data").click();
         await this.selectGroupMenuItem("Tables & Data");
-        await this.page.getByText("Smart Table").click();
+        await this.smartTableMenuItem.click();
     }
     async navigateToTooltipPage() {
         //await this.page.getByText("Modal & Overlays").click();
         await this.selectGroupMenuItem("Modal & Overlays");
-        await this.page.getByText("Tooltip").click();
+        await this.tooltipMenuItem.click();
     }
 
     async navigateToToastPage() {
         await this.selectGroupMenuItem("Modal & Overlays");
-        await this.page.getByText("Toastr").click();
+        await this.toastMenuItem.click();
     }
 
     private async selectGroupMenuItem(pageTitle: string) {
